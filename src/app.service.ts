1cc64a165c143b2debefbb11ee3d7a713f29ce6a
Banega00 <bane.stojanovic00@gmail.com>
2024-07-12 14:08:15 +0200

    feat: add server health check

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
+import packageJson from '../package.json';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  getServerHealth() {
+    return { name: packageJson.name, version: packageJson.version, status: 'UP' };
   }
 }
